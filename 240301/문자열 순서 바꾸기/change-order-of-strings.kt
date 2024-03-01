d03411013a14e9942503a4bf5edf07d2d330bac1
Jaekyung Sim <97489787+sjkyung@users.noreply.github.com>
2024-03-01 18:53:38 +0900

    Title: 문자열 순서 바꾸기, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240301/문자열 순서 바꾸기/change-order-of-strings.kt
@@ -0,0 +1,11 @@
+import java.util.Scanner
+
+fun main(args: Array<String>){
+    val sc: Scanner = Scanner(System.`in`)
+
+    var t = sc.next()
+    var s = sc.next()
+
+    println(s)
+    println(t)
+}
