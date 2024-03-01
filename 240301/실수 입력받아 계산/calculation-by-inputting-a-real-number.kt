d7f921e2005c89d7335f33ee2bded071740d1a19
Jaekyung Sim <97489787+sjkyung@users.noreply.github.com>
2024-03-01 18:20:38 +0900

    Title: 실수 입력받아 계산, Time: 167ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240301/실수 입력받아 계산/calculation-by-inputting-a-real-number.kt
@@ -0,0 +1,10 @@
+import java.util.*;
+
+fun main(args : Array<String>){
+    var sc : Scanner = Scanner(System.`in`)
+
+    var a = sc.nextDouble()
+    var b = sc.nextDouble()
+
+    print(String.format("%.2f",a+b))
+}
