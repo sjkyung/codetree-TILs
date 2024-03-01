01c1b0989f040e4f347ea3ec8f5491245ff94337
Jaekyung Sim <97489787+sjkyung@users.noreply.github.com>
2024-03-01 19:32:29 +0900

    Title: 살 수 있는 물건 2, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240301/살 수 있는 물건 2/things-able-to-buy-2.kt
@@ -0,0 +1,17 @@
+import java.util.Scanner
+
+fun main(args: Array<String>){
+    val sc: Scanner = Scanner(System.`in`)
+    var a = sc.nextInt()
+
+
+    if(a >=3000){
+        print("book")
+    }else if(a >=1000){
+        print("mask")
+    }else if(a >= 500){
+        print("pen")
+    }else{
+        print("no")
+    }
+}
