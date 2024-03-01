b819688f78bc4b79eebf751c241334e56811d85d
Jaekyung Sim <97489787+sjkyung@users.noreply.github.com>
2024-03-01 20:19:59 +0900

    Title: 별 n개 출력하기, Time: 160ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240301/별 n개 출력하기/print-n-stars.kt
@@ -0,0 +1,12 @@
+import java.util.Scanner
+
+fun main(args: Array<String>){
+    val sc: Scanner = Scanner(System.`in`)
+
+    var a =sc.nextInt()
+    var i = 0
+    while(i< a){
+        println("*")
+        i++
+    }
+}
