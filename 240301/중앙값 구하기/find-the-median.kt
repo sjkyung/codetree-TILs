1e2709e3c53be7596c3d7b8310588cd2c6f37fe1
Jaekyung Sim <97489787+sjkyung@users.noreply.github.com>
2024-03-01 20:01:25 +0900

    Title: 중앙값 구하기, Time: 132ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240301/중앙값 구하기/find-the-median.kt
@@ -0,0 +1,20 @@
+import java.util.Scanner
+
+fun main(args: Array<String>){
+    val sc: Scanner = Scanner(System.`in`)
+    var a = sc.nextInt()
+    var b = sc.nextInt()
+    var c = sc.nextInt()
+
+    if( a > b ){
+        if(a < c){
+            print(a)
+        }
+    }else{
+        if(b < c){
+            print(b)
+        }else{
+            print(c)
+        }
+    }  
+}
