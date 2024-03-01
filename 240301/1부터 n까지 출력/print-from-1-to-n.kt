a3d0235847e7366d20edaf9a557df9bc9aa264de
Jaekyung Sim <97489787+sjkyung@users.noreply.github.com>
2024-03-01 20:11:06 +0900

    Title: 1부터 n까지 출력, Time: 151ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240301/1부터 n까지 출력/print-from-1-to-n.kt
@@ -0,0 +1,10 @@
+import java.util.Scanner
+
+fun main(args: Array<String>){
+    var sc : Scanner = Scanner(System.`in`)
+    var a = sc.nextInt()
+
+    for(i in 1..a){
+        print("$i ")
+    }
+}
