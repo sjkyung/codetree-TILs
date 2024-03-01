f87d7f97aaf5b5448e739e8ec08ff5947b990d0c
Jaekyung Sim <97489787+sjkyung@users.noreply.github.com>
2024-03-01 19:21:38 +0900

    Title: 정사각형의 넓이, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240301/정사각형의 넓이/area-of-a-rectangle.kt
@@ -0,0 +1,16 @@
+import java.util.Scanner
+
+fun main(args: Array<String>){
+    val sc: Scanner = Scanner(System.`in`)
+
+    var a = sc.nextInt()
+
+    if(a >= 5){
+        println(a*a)
+    }else{
+        println(a*a)
+        println("tiny")
+    }
+
+
+}
