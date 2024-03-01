d2854cf9b1c3b128a6c4402354aa26b76f2b0bf0
Jaekyung Sim <97489787+sjkyung@users.noreply.github.com>
2024-03-01 18:50:54 +0900

    Title: 입력받은 값과 합 출력, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240301/입력받은 값과 합 출력/output-of-value-and-sum.kt
@@ -0,0 +1,12 @@
+import java.util.*;
+
+fun main(args: Array<String>){
+    var sc: Scanner = Scanner(System.`in`)
+    
+    var a = sc.nextInt()
+    var b = sc.nextInt()
+    var c = a+b
+
+    println("$a $b $c")
+
+}
