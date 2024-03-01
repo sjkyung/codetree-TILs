86105ee2eebd05508baf6d15f0df6042b64836fb
Jaekyung Sim <97489787+sjkyung@users.noreply.github.com>
2024-03-01 18:31:05 +0900

    Title: 정수 세 개 입력받아 출력, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240301/정수 세 개 입력받아 출력/take-three-integers-and-output.kt
@@ -0,0 +1,11 @@
+import java.util.*;
+
+fun main(args : Array<String>){
+    var sc : Scanner = Scanner(System.`in`)
+
+    var a = sc.nextInt()
+    var b = sc.nextInt()
+    var c = sc.nextInt()
+
+    print("$a $b $c")
+}
