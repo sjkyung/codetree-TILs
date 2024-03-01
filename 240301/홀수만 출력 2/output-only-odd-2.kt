f7523a9445202efe78de8c93985908dace0cde21
Jaekyung Sim <97489787+sjkyung@users.noreply.github.com>
2024-03-01 20:15:57 +0900

    Title: 홀수만 출력 2, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240301/홀수만 출력 2/output-only-odd-2.kt
@@ -0,0 +1,14 @@
+import java.util.Scanner
+
+fun main(args: Array<String>){
+    val sc: Scanner = Scanner(System.`in`)
+
+    var a =sc.nextInt()
+    var c =sc.nextInt()
+
+    for(i in a downTo c){
+        if(i%2 ==1){
+            print("$i ")
+        }
+    }
+}
