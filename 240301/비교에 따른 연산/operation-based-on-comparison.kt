bf7c902efca17b87fca64456076b1d05426f9ea9
Jaekyung Sim <97489787+sjkyung@users.noreply.github.com>
2024-03-01 19:24:39 +0900

    Title: 비교에 따른 연산, Time: 129ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240301/비교에 따른 연산/operation-based-on-comparison.kt
@@ -0,0 +1,14 @@
+import java.util.Scanner
+
+fun main(args: Array<String>){
+    val sc: Scanner = Scanner(System.`in`)
+
+    var a = sc.nextInt()
+    var b = sc.nextInt()
+
+    if(a >b){
+        print(a*b)
+    }else{
+        print(b%a)
+    }
+}
